1c28443daa56d8bec68c635b7384cb95e4d0142d
broadbear <broadbear@broadbear-PC>
2013-11-30 23:21:50 -0600

    Started A*.

--- graph/src/org/mike/graph/SequentialGraph.java
+++ graph/src/org/mike/graph/SequentialGraph.java
@@ -1,7 +1,11 @@
 package org.mike.graph;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 
 public class SequentialGraph {
 	Graph graph;
@@ -121,5 +125,62 @@ public class SequentialGraph {
 			}
 		}
 		return pi;
+	}
+
+	/**
+	 * A* single shortest path algorithm. pi (previous) array has
+	 * been substituted for the 'came_from' map.
+	 * 
+	 * @param start
+	 * @param goal
+	 * @return
+	 */
+	Integer[] aStarShortestPath(int start, int goal) {
+		Set<Integer> closedSet = new HashSet<Integer>();
+		Set<Integer> openSet = new HashSet<Integer>();
+		Integer[] gScore = new Integer[graph.size()];
+		Integer[] fScore = new Integer[graph.size()];
+
+		openSet.add(start);
+		gScore[start] = 0;
+		fScore[start] = gScore[start] + heuristicCostEstimate(start, goal);
+		while(!openSet.isEmpty()) {
+			Integer current = getLowestFScore(openSet, fScore);
+			if (current == goal) {
+				return pi;
+			}
+			openSet.remove(current);
+			closedSet.add(current);
+			for (Integer neighbor: graph.outEdges(current)) {
+				Integer tentativeGScore = gScore[current] + graph.getWeight(current, neighbor);
+				Integer tentativeFScore = tentativeGScore + heuristicCostEstimate(neighbor, goal);
+				if (closedSet.contains(neighbor) && tentativeFScore >= fScore[neighbor]) {
+					continue;
+				}
+				if (!openSet.contains(neighbor) || tentativeFScore < fScore[neighbor]) {
+					pi[neighbor] = current;
+					gScore[neighbor] = tentativeGScore;
+					fScore[neighbor] = tentativeFScore;
+					if (!openSet.contains(neighbor)) {
+						openSet.add(neighbor);
+					}
+				}
+			}
+		}		
+		return pi;
+	}
+	
+	int heuristicCostEstimate(int start, int goal) {
+		return 1; // TODO: 
+	}
+	
+	Integer getLowestFScore(Set<Integer> openSet, Integer[] fScore) {
+		Integer lowest = Integer.MAX_VALUE;
+		for (Integer i: openSet) {
+			if (fScore[i] < fScore[lowest]) {
+				lowest = i;
+			}
+		}
+		return lowest;
 	}
 }
